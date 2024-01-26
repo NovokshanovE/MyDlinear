f7c43788f69a68a278d527c52754bc162d3d8bed
Evgeny Novokshanov <evgeny.novokshanov@gmail.com>
2024-01-26 14:12:52 +0300

    Изменена структура файлов. Последняя версия модели переведена в python script.

--- RollingWindow/test.py
+++ RollingWindow/test.py
@@ -2,12 +2,18 @@ from pandas import read_csv
 from sklearn.metrics import mean_squared_error
 from matplotlib import pyplot
 from numpy import mean
+from matplotlib import dates
 # load data
-series = read_csv('ETTh1.csv', header=0, index_col=0,usecols=['date', 'HUFL'])
+series = read_csv('ETTh1.csv', usecols=['date', 'HUFL'])
 # prepare data
-X = series.values
-w = 40
-train, test = X[0:-2400:10], X[-2400::10]
+X = series.values[:,1]
+data = series.values[:,0,]
+
+w = 4
+fmt = dates.DateFormatter('%H:%M:%S')
+train, test = X[0:2400:24], X[2400::24]
+data_test = data[2400::24,]
+print(data_test)
 # walk-forward validation
 history = [x for x in train]
 predictions = list()
@@ -18,7 +24,9 @@ for i in range(len(test)):
 	# observation
 	history.append(test[i])
 # plot predictions vs observations
-pyplot.plot(test)
-pyplot.plot(predictions)
+pyplot.plot(test, label='реальные значения')
+pyplot.plot(predictions, label='прогноз')
+pyplot.xlabel("Дни")
+pyplot.ylabel("HUFL")
 pyplot.savefig(f"w{w}", dpi=200)
 pyplot.show()
