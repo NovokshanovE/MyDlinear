0d415208f051f8bcfd99be7b8d226776e02fe0e1
zhenya.novokshanov <evgeny.novokshanov@gmail.com>
2024-02-24 20:22:34 +0300

    test

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # import torch
 import random
 from Dlinear_v2.MyDLinear import DLinear
-
+import datetime
 import matplotlib.pyplot as plt
 # from matplotlib.pyplot import figure
 import numpy as np
@@ -13,24 +13,24 @@ from statsmodels.tsa.seasonal import STL
 
 def func(x):
     return 0.01*np.sin(x/10)#1.3*x+10#np.sin(x)/100
-data_set = 11000
+data_set = 4000
 input_size = 100
 output_size = 100
 learning_rate = 0.001
 step = 1
-data_size = 6000
-column_name = 'HUFL'
-dataset_name = 'ETTh1'
+data_size = 3000
+column_name = 'value'
+dataset_name = 'dataset'
 dLinear = DLinear(data_set, input_size, output_size, step = 1, data_size = data_size, column_name=column_name, dataset_name = dataset_name)
 data = dLinear.data_reader(file_name=dataset_name +'.csv', column_name=column_name)
-
+# data = None
 # print(summary(dLinear))
 def train():
     #data = dLinear.set_data(func=func)
     dLinear.set_model(type="ma")
-    #dLinear.load_modal("dlinear(stl+rw_v2)_EUR-GBP_Value_input100_output100")
+    dLinear.load_modal("dlinear(2024-02-23 16:54:53.039394)_dataset_1_value_input100_output100MA")
     # dLinear.train__with_metrics(data_set=data_set, num_epochs=1000)
-    dLinear.train(num_epochs =  1000, gpu=True)
+    # dLinear.train(num_epochs =  1000, gpu=True)
 
 
 
@@ -53,7 +53,7 @@ def test1():
     plt.ylabel(column_name, fontsize=14)
     dLinear.MAE(data_set=data_set)
     dLinear.MAPE(data_set=data_set)
-    plt.savefig(f'results/rw_results/{dataset_name}_{column_name}_test1_ma')
+    plt.savefig(f'results/rw_results/{dataset_name}_{column_name}_test_{datetime.datetime.now().date()}_one_layer')
     # plt.show()
     #plt.savefig(model_name+"2", dpi=1000)
     
